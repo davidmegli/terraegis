0306152c25fde9d8792c722d63416038c71e4fbb
davidmegli <verbatimdvd91@gmail.com>
2024-08-13 20:28:37 +0200

    Initial commit

---
+++ src/main/java/com/terraegis/terraegis/TerrAegisApplication.java
@@ -0,0 +1,13 @@
+package com.terraegis.terraegis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrAegisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerrAegisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/terraegis/terraegis/TerrAegisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terraegis.terraegis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrAegisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
